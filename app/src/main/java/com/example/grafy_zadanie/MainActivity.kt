c24d9eeeb361a000d0b3b85fabf76f46cba8cc2e
Filippo0420 <fpawelec0@gmail.com>
2023-09-22 13:27:16 +0200

    Ostatnie dodatki do kodu

--- app/src/main/java/com/example/grafy_zadanie/MainActivity.kt
+++ app/src/main/java/com/example/grafy_zadanie/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.grafy_zadanie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.SeekBar
 
@@ -9,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         findViewById<SeekBar>(R.id.pasek).min = 1
 
         findViewById<ImageView>(R.id.iV1).setOnClickListener {
@@ -28,6 +30,14 @@ class MainActivity : AppCompatActivity() {
         }
 
         findViewById<ImageView>(R.id.iV5).setOnClickListener {
+
+        }
+
+        findViewById<Button>(R.id.btn_waga).setOnClickListener {
+
+        }
+
+        findViewById<Button>(R.id.btn_znajdz).setOnClickListener {
 
         }
     }
